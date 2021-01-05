bccea508054ad766496c29763ce8df14b05ef2d8
Ami <30578166+amisama@users.noreply.github.com>
2021-01-05 02:38:53 -0800

    Update wallpaper.php

--- wallpaper.php
+++ wallpaper.php
@@ -16,7 +16,13 @@ function mycurl($url)
 
 }
 $maqlo = $_GET['q'];
-$url = "https://wall.alphacoders.com/search.php?search=".$maqlo;
+$page = $_GET['page'];
+if(!isset($page)){
+    $page = 1;
+} else {
+    $page = $page;
+}
+$url = "https://wall.alphacoders.com/search.php?search=".$maqlo."&page=".$page;
 $data = mycurl($url);
 require 'simple_html_dom.php';
 $html = new simple_html_dom();
@@ -24,15 +30,17 @@ $html->load($data);
 //echo $data;die;
 $gambar = $html->find('div[class=thumb-container-big ]');
 foreach ($gambar as $key => $value) {
-	$image = $value->find('img',0)->{'data-src'};
+    $image = $value->find('img',0)->{'src'};
 	$title = $value->find('a',0)->title;
 	$results['status'] = 'success';
-	$results['creator'] = 'Ami';
+	$results['creator'] = 'ami';
 	$results['results'][] = array (
-		'image' => str_replace('350','1920',$image), //image          
+		'image' => array (
+		    'lite' => $image,
+		    'hd' => str_replace('350','1920',$image)
+		    ),      
 		'title' => $title //title
 	);
-}
+}  
 echo json_encode($results, JSON_PRETTY_PRINT);
 //var_dump($gambar);
-//print_r($mmek);
